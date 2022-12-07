322c8ee610d13d5f4977858a98ba51fd79610d1a
Ana Clara <anaclaranoronha.m@gmail.com>
2022-12-07 15:29:20 -0300

    07122022

---
+++ src/app/produtos/detalhes-produto/detalhes-produto.component.ts
@@ -0,0 +1,43 @@
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CarrinhoService } from 'src/app/carrinho.service';
+import { NotificacaoService } from 'src/app/notificacao.service';
+import { IProduto, IProdutoCarrinho } from 'src/app/produtos';
+import { ProdutosService } from 'src/app/produtos.service';
+
+@Component({
+  selector: 'app-detalhes-produto',
+  templateUrl: './detalhes-produto.component.html',
+  styleUrls: ['./detalhes-produto.component.css']
+})
+export class DetalhesProdutoComponent implements OnInit {
+
+  produto: IProduto | undefined;
+  quantidade = 1;
+
+  constructor(
+    private produtoService: ProdutosService,
+    private route: ActivatedRoute,
+    private notificacaoService: NotificacaoService,
+    private carrinhoService: CarrinhoService
+  ) { }
+
+  ngOnInit(): void {
+
+    const routeParams = this.route.snapshot.paramMap;
+    const produtoId = Number(routeParams.get("id"));
+    console.log(produtoId);
+    this.produto = this.produtoService.getOne(produtoId);
+  }
+
+  adicionarAoCarrinho(){
+    this.notificacaoService.notificar("O produto foi adicionado ao seu carrinho!");
+    const produto: IProdutoCarrinho = {
+      ...this.produto!,
+      quantidade: this.quantidade
+    }
+    this.carrinhoService.adicionarAoCarrinho(produto);
+  }
+
+}
